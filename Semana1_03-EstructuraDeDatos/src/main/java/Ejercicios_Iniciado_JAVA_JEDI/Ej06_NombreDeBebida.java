6616060706b7531aa0b3552d12cebc68fd8082d2
FedeBazan <juliopablofedericobazan@gmail.com>
2023-09-19 12:52:48 -0300

    Ejercicios Informatorio JAVA-Iniciado

--- Semana1_03-EstructuraDeDatos/src/main/java/Ejercicios_Iniciado_JAVA_JEDI/Ej06_NombreDeBebida.java
+++ Semana1_03-EstructuraDeDatos/src/main/java/Ejercicios_Iniciado_JAVA_JEDI/Ej06_NombreDeBebida.java
@@ -1,7 +1,18 @@
 package Ejercicios_Iniciado_JAVA_JEDI;
 
+import java.util.Scanner;
+
 public class Ej06_NombreDeBebida {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String nombreMascota;
+        String ultimaPelicula;
+        System.out.print("Ingrese el nombre de su última mascota: ");
+        nombreMascota = scanner.next();
+        //con scanner.nextLine() la variable puede tomar una linea de texto, de esta forma tambien puede incluirse los " "
+        scanner.nextLine(); // Limpiar el scanner para una nueva linea
+        System.out.print("Ingrese el nombre de la última película: ");
+        ultimaPelicula = scanner.nextLine();
+        System.out.println("\nNombre de su mascota: " + nombreMascota + "\nNombre película: " + ultimaPelicula);
     }
 }
