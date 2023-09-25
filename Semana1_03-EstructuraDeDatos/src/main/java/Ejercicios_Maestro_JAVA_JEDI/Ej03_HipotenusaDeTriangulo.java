9b9058836b6a8c8b0b7597c2456943b9fa00f190
FedeBazan <juliopablofedericobazan@gmail.com>
2023-09-25 13:00:41 -0300

    Ejercicios Informatorio Java Semana 1

--- Semana1_03-EstructuraDeDatos/src/main/java/Ejercicios_Maestro_JAVA_JEDI/Ej03_HipotenusaDeTriangulo.java
+++ Semana1_03-EstructuraDeDatos/src/main/java/Ejercicios_Maestro_JAVA_JEDI/Ej03_HipotenusaDeTriangulo.java
@@ -1,7 +1,22 @@
 package Ejercicios_Maestro_JAVA_JEDI;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Ej03_HipotenusaDeTriangulo {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat formato = new DecimalFormat("0.00");
+
+        float catA,catB;
+        double hip;
+
+        System.out.print("Ingrese el valor de un cateto: ");
+        catA=scanner.nextFloat();
+        System.out.print("Ingrese el valor del otro cateto: ");
+        catB= scanner.nextFloat();
+        hip=((catA*catA)+(catB*catB));
+        hip=Math.sqrt(hip); //Math.sqrt(double) , se usa para sacar la raiz cuadrada de X numero
+        System.out.println("El valor de la Hipotenusa es: "+formato.format(hip));
     }
 }
---
+++ Semana1_03-EstructuraDeDatos/src/main/java/Ejercicios_Maestro_JAVA_JEDI/Ej04_CalculoDeLaMediaAritmetica.java
@@ -0,0 +1,7 @@
+package Ejercicios_Maestro_JAVA_JEDI;
+
+public class Ej04_CalculoDeLaMediaAritmetica {
+    public static void main(String[] args) {
+
+    }
+}
