2c490f56ae12e8bd8076fe487351a4c30e6ea9b0
FedeBazan <juliopablofedericobazan@gmail.com>
2023-09-26 09:13:39 -0300

    Ejercicios Informatorio Java Semana 2

--- semana2programacionestructurada/src/main/java/iniciado/Ej01TrianguloDeAsteriscos.java
+++ semana2programacionestructurada/src/main/java/iniciado/Ej01TrianguloDeAsteriscos.java
@@ -1,11 +1,27 @@
 package iniciado;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ej01TrianguloDeAsteriscos {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int filas;
+        //Bucle aplicando un control de varible con Do-While y manejo de Exepciones Try-Catch
+        do {
+            try {
+                System.out.print("\nIngrese numero de filas:");
+                filas=scanner.nextInt();
+                break;
+            }catch (InputMismatchException e){
+                System.out.println("ERROR-SE INGRESO UN VALOR INOPERABLE");
+                System.out.print("Se aconseja agregar un numero ENTER POSITIVO");
+                scanner.nextLine();
+            }
+        }while (true);
+        /*
+        El bucle a continuacion es sin exepciones y tira un error frenando la ejecucion de codigo
+
         System.out.print("Ingrese numeros de filas para el Triangulo: ");
         filas = scanner.nextInt();
 
@@ -16,7 +32,7 @@ public class Ej01TrianguloDeAsteriscos {
             System.out.print("Ingresar un numero mayor a 0: ");
             filas = scanner.nextInt();
         }
-
+        */
         //Triangulo de Arriba hacia Abajo
         System.out.println("Triangulo\n");
         for (int i=0;i<filas;i++){
