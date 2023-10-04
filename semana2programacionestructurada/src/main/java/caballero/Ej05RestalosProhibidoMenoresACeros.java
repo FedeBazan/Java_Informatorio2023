e51dd203ad01f7eb1124a393cf692dee651ebcaa
FedeBazan <juliopablofedericobazan@gmail.com>
2023-10-03 21:52:12 -0300

    Update Ej05RestalosProhibidoMenoresACeros.java

--- semana2programacionestructurada/src/main/java/caballero/Ej05RestalosProhibidoMenoresACeros.java
+++ semana2programacionestructurada/src/main/java/caballero/Ej05RestalosProhibidoMenoresACeros.java
@@ -2,6 +2,7 @@ package caballero;
 
 public class Ej05RestalosProhibidoMenoresACeros {
     public static void main(String[] args) {
-
+        double numDec[]={2.1,1.8,3.6,0.1};
+        double numDec2[]={1.2,8.1,6.3,1.0};
     }
 }
